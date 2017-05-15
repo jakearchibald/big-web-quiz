c50f220d13670fc2f000615a41af3f4290ea9686
Jake Archibald <jaffathecake@gmail.com>
2017-05-15 09:38:09 -0700

    Moving links list into its own file

--- shared/components/app.js
+++ shared/components/app.js
@@ -23,6 +23,7 @@ import QuestionWaiting from './question-waiting';
 import LoginStatus from './login-status';
 import Question from './question';
 import Transition from './transition';
+import EndScreen from './end-screen';
 import LongPoll from '../long-poll';
 
 export default class App extends BoundComponent {
@@ -104,18 +105,7 @@ export default class App extends BoundComponent {
         </header>
         <div class="container">
           {showEndScreen ?
-            <div class="end-screen">
-              Thanks for playing! Here are some links:
-              <ul class="end-screen__list">
-                <li class="end-screen__list-item">
-                  <a class="end-screen__list-item-link" href="https://jakearchibald.com/2016/sounds-fun/">How we did the sound</a>
-                </li>
-
-                <li class="end-screen__list-item">
-                  <a class="end-screen__list-item-link" href="https://aerotwist.com/blog/anatomy-of-a-frame/">Anatomy of a frame</a>
-                </li>
-              </ul>
-            </div>
+            <EndScreen />
             :
             (user ?
               (shouldShowQuestion?
---
+++ shared/components/end-screen.js
@@ -0,0 +1,16 @@
+import { h } from 'preact';
+
+export default () => (
+  <div class="end-screen">
+    Thanks for playing! Here are some links:
+    <ul class="end-screen__list">
+      <li class="end-screen__list-item">
+        <a class="end-screen__list-item-link" href="https://jakearchibald.com/2016/sounds-fun/">How we did the sound</a>
+      </li>
+
+      <li class="end-screen__list-item">
+        <a class="end-screen__list-item-link" href="https://aerotwist.com/blog/anatomy-of-a-frame/">Anatomy of a frame</a>
+      </li>
+    </ul>
+  </div>
+);
