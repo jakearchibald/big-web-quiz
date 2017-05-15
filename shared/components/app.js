4bdf376de9fec43bc4370a53fe1fcad6cf90bccb
Paul Lewis <github@aerotwist.com>
2017-05-15 04:50:55 -0700

    Styles EOQ screen

--- shared/components/app.js
+++ shared/components/app.js
@@ -106,9 +106,13 @@ export default class App extends BoundComponent {
           {showEndScreen ?
             <div class="end-screen">
               Thanks for playing! Here are some links:
-              <ul>
-                <li>
-                  <a href="https://jakearchibald.com/2016/sounds-fun/">How we did the sound</a>
+              <ul class="end-screen__list">
+                <li class="end-screen__list-item">
+                  <a class="end-screen__list-item-link" href="https://jakearchibald.com/2016/sounds-fun/">How we did the sound</a>
+                </li>
+
+                <li class="end-screen__list-item">
+                  <a class="end-screen__list-item-link" href="https://aerotwist.com/blog/anatomy-of-a-frame/">Anatomy of a frame</a>
                 </li>
               </ul>
             </div>
